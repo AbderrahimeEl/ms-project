dc685fd357db79eac5d0a36496d8c653aa0c134f
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-09-21 10:23:02 +0100

    Add stride-leaderboard-service module and update Docker Compose configuration

---
+++ stride-leaderboard-service/src/main/java/com/elm/strideleaderboardservice/StrideLeaderboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elm.strideleaderboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrideLeaderboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrideLeaderboardServiceApplication.class, args);
+	}
+
+}
